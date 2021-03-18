b18a8fc75542735cbf0ed984d0536badaa74a3af
rob dewhirst <3137089+robdew@users.noreply.github.com>
2021-03-17 22:58:52 -0400

    changed regex to match IDs for managed instances or standard instances

--- ssm_tools/resolver.py
+++ ssm_tools/resolver.py
@@ -137,7 +137,7 @@ class InstanceResolver():
 
     def resolve_instance(self, instance):
         # Is it a valid Instance ID?
-        if re.match('^i-[a-f0-9]+$', instance):
+        if re.match('^m?i-[a-f0-9]+$', instance):
             return instance
 
         # It is not - find it in the list
